2ab8e0f1229bd3f8f442e1da296353cfc3c0f3e1
sohorabsajib <sohorab15-9582@diu.edu.bd>
2022-02-17 05:29:31 +0600

    2nd commit

--- main.js
+++ main.js
@@ -7,10 +7,12 @@ const calculate =  document.getElementById('calculate').addEventListener('click'
     const clothes =parseInt(document.getElementById("clothes").value);
     const incomeInput = parseInt(document.getElementById("income").value);
     const expenses = (food + rent + clothes);
-    const balance = incomeInput- expenses;
+    const balance = parseInt(incomeInput- expenses);
+    
     
     const totalExpenses = document.getElementById('totalExpenses').innerText = expenses;
     const totalbalance = document.getElementById('balance').innerText = balance;
+    
     
 
 
@@ -18,8 +20,14 @@ const calculate =  document.getElementById('calculate').addEventListener('click'
 });
 const save =  document.getElementById('save').addEventListener('click',function(){
     const persentage = parseInt(document.getElementById('persentage').value);
-    const finalpersentage = persentage/100;
+    const income = parseInt(document.getElementById("income").value);
+
+    const finalpersentage = parseInt(persentage * (income / 100));
     const saving = document.getElementById('saving').innerText = finalpersentage;
-    const remain = document.getElementById('remain').innerText = totalbalance - finalpersentage;
+    
+    
+    const remaingBalance = (balance - finalpersentage);
+    
+    const remainss = document.getElementById('remain').innerText = remaingBalance;
 
 });
